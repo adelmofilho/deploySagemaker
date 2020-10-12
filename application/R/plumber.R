ea72e2652dd0bc4ec57f8ac4d049ae7c513d79ee
adelmofilho <adelmo.aguiar.filho@gmail.com>
2020-10-11 21:38:55 -0300

    repo organizado

---
+++ application/R/plumber.R
@@ -0,0 +1,30 @@
+# plumber.R
+
+#' Ping to show server is there
+#' @get /ping
+function() {
+    return('')}
+
+#' Parse input and return prediction from model
+#' @param req The http request sent
+#' @post /invocations
+function(req) {
+    return('ok')
+    # # Setup locations
+    # prefix <- '/opt/ml'
+    # model_path <- paste(prefix, 'model', sep='/')
+
+    # # Bring in model file and factor levels
+    # load(paste(model_path, 'mars_model.RData', sep='/'))
+
+    # # Read in data
+    # conn <- textConnection(gsub('\\\\n', '\n', req$postBody))
+    # data <- read.csv(conn)
+    # close(conn)
+
+    # # Convert input to model matrix
+    # scoring_X <- model.matrix(~., data, xlev=factor_levels)
+
+    # # Return prediction
+    # return(paste(predict(mars_model, scoring_X, row.names=FALSE), collapse=','))
+}
